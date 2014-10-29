ff13b8e3e350cd1d4671beffdc7534db23fcf058
Mikayla <mikayla.thompson@yale.edu>
2014-10-28 23:52:09 -0400

    Login form added

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
-
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'name': 'Mikayla'}
+    return render_template('index.html',
+                    user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Logging in %s with pass %s.'%
+                (form.username.data, form.password.data))
+        return redirect('/index')
+    return render_template('login.html',
+                            title='Login',
+                            form=form)
