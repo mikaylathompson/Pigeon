25c3e5aec65f9b2065c1e4dc728811a8e0c462d2
Mikayla <mikayla.thompson@yale.edu>
2014-10-29 00:01:31 -0400

    Signup form added

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,15 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import Required, EqualTo
 
 class LoginForm(Form):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    username = StringField('username', [Required()])
+    password = PasswordField('password', [Required()])
+
+class SignupForm(Form):
+    username = StringField('username', [Required()])
+    password = PasswordField('password', 
+            [Required(), EqualTo('confirm', message='Passwords must match.')])
+    confirm = PasswordField('confirm')
 
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect
 from app import app
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
 
 
 @app.route('/')
@@ -19,4 +19,14 @@ def login():
         return redirect('/index')
     return render_template('login.html',
                             title='Login',
+                            form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash('Sign up: %s, %s'%(form.username.data, form.password.data))
+        return redirect('/index')
+    return render_template('signup.html',
+                            title='Sign up',
                             form=form)
