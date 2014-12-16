f3987d5a58fc2cc103df676a584b0e2c5764549d
Mikayla <mikayla.thompson@yale.edu>
2014-12-15 21:25:34 -0500

    Removed friend class

--- app/models.py
+++ app/models.py
@@ -7,8 +7,6 @@ class User(db.Model):
     passhash = db.Column(db.String(512))
     publickey = db.Column(db.String(1024))
     privatekey = db.Column(db.String(1024))
-    recMessages = db.relationship('Message', backref='receivedMessages', lazy='dynamic')
-    friendships = db.relationship('Friend', backref='friendships', lazy='dynamic')
 
     def is_authenticated(self):
         return True
@@ -26,27 +24,20 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(8000))
     sent = db.Column(db.DateTime)
-    encryp = db.Column(db.Boolean)
     read = db.Column(db.Boolean)
-
-    destID = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    destName = db.Column(db.String(64))
     originName = db.Column(db.String(64))
-    #originID = db.Column(db.Integer, db.ForeignKey('user.id'))
-    #sender = db.relationship('User', foreign_keys='Message.originID')
+
+    def json(self):
+        j = {}
+        j['id'] = self.id
+        j['text'] = self.text
+        j['sent'] = self.sent
+        j['read'] = self.read
+        j['destName'] = self.destName
+        j['originName'] = self.originName
+        return j
 
     def __repr__(self):
         return '<Message: %r->%r@%r>'%(self.originID, self.destID, self.sent)
-
-class Friend(db.Model):
-    __tablename__ = 'friend'
-    id = db.Column(db.Integer, primary_key=True)
-    ownerID = db.Column(db.Integer, db.ForeignKey('user.id'))
-    friendName = db.Column(db.String(64))
-    #friendID = db.Column(db.Integer, db.ForeignKey('user.id'))
-    #friend = db.relationship('User', foreign_keys='Friend.friendID')
-    idverified = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return '<Friend %r of %r>'%(self.friendID, self.ownerID)
 
